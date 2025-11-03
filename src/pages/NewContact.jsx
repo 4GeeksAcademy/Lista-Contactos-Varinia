92c06e70c996adbbdd0d53869154110d32b5e058
Varinia <v.mesias.r@gmail.com>
2025-11-03 21:14:48 +0000

    valores al formulario

--- src/pages/NewContact.jsx
+++ src/pages/NewContact.jsx
@@ -4,26 +4,28 @@ import { Link } from 'react-router-dom';
 
 export const NewContact = () => {
     const { store, dispatch } = useGlobalReducer()
-    let [form, setForm] = useState ("")
+    let [data, setData] = useState ({
+        name: "", email: "", phone: "", address: "", 
+    })
 
     return (
-        <div>
-            <h1>aqui iran los contactos</h1>
+        <div className='container'>
+            <h1>Add a new Contact</h1>
             <div className="mb-3">
-                <label for="formGroupExampleInput" className="form-label">Full Name</label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Full Name" />
+                <label htmlfor="formGroupExampleInput" className="form-label">Full Name</label>
+                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Full Name" value={data.name}/>
             </div>
             <div className="mb-3">
-                <label for="formGroupExampleInput2" className="form-label">Email</label>
-                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Enter Email" />
+                <label htmlfor="formGroupExampleInput2" className="form-label">Email</label>
+                <input type="email" className="form-control" id="formGroupExampleInput2" placeholder="Enter Email" value={data.email} />
             </div>
             <div className="mb-3">
-                <label for="formGroupExampleInput" className="form-label">Phone</label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Enter Phone" />
+                <label htmlfor="formGroupExampleInput" className="form-label">Phone</label>
+                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Enter Phone" value={data.phone}/>
             </div>
             <div className="mb-3">
-                <label for="formGroupExampleInput" className="form-label">Address</label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Enter Address" />
+                <label htmlfor="formGroupExampleInput" className="form-label">Address</label>
+                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Enter Address" value={data.address}/>
             </div>
             <div className="d-grid gap-2">
                 <button className="btn btn-secondary" type="button">Save</button>
